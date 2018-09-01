9320c42cce5e280e12bd416ca3e9d386b5a4f9cd
Kara Isreal <isreal.kara92@gmail.com>
2018-09-01 05:53:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const two = 2
 
 var theFunk = 'FUNKY!'
  function funkyFunction() {
-  return(theFunk);
+  return('theFunk');
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
